e4c3c526602570eae1c8f5b618a49225d0bd323e
goatpig <moothecowlord@gmail.com>
2016-09-21 14:05:32 -0400

    added listening to the socket

--- cppForSwig/gtest/socketTesting.cpp
+++ cppForSwig/gtest/socketTesting.cpp
@@ -1,8 +1,13 @@
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <fcntl.h>
 #include <iostream>
 #include <errno.h>
+#include <string.h>
+#include <string>
 
 using namespace std;
 
@@ -30,7 +35,7 @@ int main(void)
       return 0;
    }
 
-   struct flock lock;
+   /*struct flock lock;
    lock.l_type = F_WRLCK;
    lock.l_start = 0;
    lock.l_whence = SEEK_SET;
@@ -39,6 +44,37 @@ int main(void)
    if(fcntl(sockfd, F_SETLKW, &lock) != 0)
    {
       cout << "failed to lock socket with errno: " << errno << endl;
+      return 0;
+   }*/
+
+   string host("127.0.0.1");
+   unsigned long tcp_ia = inet_addr(host.c_str());
+
+   struct sockaddr_in sa;
+   memset(&sa, 0, sizeof(sa));
+
+   sa.sin_family = AF_INET;
+   sa.sin_port   = htons(9999);
+   sa.sin_addr.s_addr = tcp_ia;
+
+   if(bind(sockfd, (struct sockaddr*)&sa, sizeof(sa)) < 0)
+   {
+      cout << "failed to bind socket with errno: " << errno << endl;
+      return 0;
+   }
+   
+   if(listen(sockfd, 10) < 0)
+   {
+      cout << "failed to listen on socket with errno: " << errno << endl;	
+      return 0;
+   }
+
+   struct sockaddr_in accept_sa;
+   socklen_t socklen = sizeof(accept_sa);
+
+   if(accept(sockfd, (struct sockaddr*)&accept_sa, &socklen) < 0)
+   {
+      cout << "failed to accept on socket with errno: " << errno << endl;
       return 0;
    }
 
